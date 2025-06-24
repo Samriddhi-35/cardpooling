9e6f83ebf8e58d13d4e4cfc57ee83cb8a2822211
Samriddhi-35 <samriddhisharma632@gmail.com>
2025-06-24 11:37:54 +0530

    add docker file

--- backend/apps/db.py
+++ backend/apps/db.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:admin@localhost:5432/card"
+import os
+
+DATABASE_URL = os.environ.get(
+	"DATABASE_URL", 
+	"postgresql://admin:admin@localhost:5432/cardpooling"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
